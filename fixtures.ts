0983f83db75ad0a4d038a70313b2ec37538d8cf7
Asifa Khan <asifaxikhan@gmail.com>
2026-06-27 12:13:52 -0700

    add data-driven login test

--- fixtures.ts
+++ fixtures.ts
@@ -4,13 +4,12 @@ import { LoginPage } from './pages/LoginPage';
 type MyFixtures = { loggedInPage : void; };
 
 export const test = base.extend<MyFixtures>({
-    loggedInPage: async ({page}, use) => {
+    loggedInPage: async ({ page }, use) => {
         const loginPage = new LoginPage(page);
         await loginPage.goto();
         await loginPage.login('standard_user', 'secret_sauce');
         await use(page);
-    }
-
+    },
 });
 
 
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -20,10 +20,32 @@ test.describe('Login Page Tests', () => {
 
     });
 
+   /*
     test('should show error with invalid credentials', async ({page}) => {
          await loginPage.login('wrong_user', 'wrong_pass');
          await expect(page.locator('[data-test="error"]')).toBeVisible();
     });
 
+    test('should show error with correct user name and wrong password', async ({page})=> {
+        await loginPage.login('standard_user', 'wrong_pass');
+        await expect(page.locator('[data-test="error"]')).toBeVisible();
+    });
 
+    test('should show error with empty credentials', async ({ page }) => {
+         await loginPage.login('', '');
+         await expect(page.locator('[data-test="error"]')).toBeVisible();
+    });
+    */
+
+    const badCredentials = [ {username: 'wrong_user' , password: 'wrong_pass' },
+                            {username: 'standard_user', password: 'wrong_pass'},
+                            {username: '', password: ''}, ];
+
+    for(const creds of badCredentials){
+        test(`should show error for username + "${creds.username}"`, async({page})=>{
+            await loginPage.login(creds.username, creds.password);
+            await expect(page.locator('[data-test="error"]')).toBeVisible();
+        });
+    }
+    
 });
