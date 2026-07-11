de4f40bd04cb158865c60a83da15a90c025be2a2
Asifa Khan <asifaxikhan@gmail.com>
2026-07-10 20:16:56 -0700

    Fix visual snapshot for CI

--- tests/visual.spec.ts
+++ tests/visual.spec.ts
@@ -1,13 +1,6 @@
 import { test, expect } from '../fixtures';
 
-test.describe('Visual Tests', () => {
-
-    test.beforeEach(async ({ page, loggedInPage }) => {
-        await page.goto('https://www.saucedemo.com/inventory.html');
-    });
-
-    test('products page matches snapshot', async ({ page }) => {
-        await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.1 });
-    });
-
+test('products page matches snapshot', async ({ page, loggedInPage }) => {
+    await page.goto('https://www.saucedemo.com/inventory.html');
+    await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.1 });
 });
